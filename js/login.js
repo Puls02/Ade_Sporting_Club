2c2b4e05dfa2f50cbaa3d2b5e8f777ab033039ab
Puls02 <andrea.puls17@gmail.com>
2024-04-23 10:28:20 +0200

    Aggiunto menù a tendina di login, ma voglio modificarlo

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 //Apro il menù laterale
-const show_menu= document.querySelector('.person');
+/*const show_menu= document.querySelector('.person');
 const nav=document.querySelector('.login_menu');
 
 show_menu.onclick=()=>{
-    show_menu.classList.toggle('login_menu');
-};
+    nav.classList.toggle("show");
+};*/
