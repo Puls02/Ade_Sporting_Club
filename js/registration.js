fde7be83a9e90c22def5b93c43acd97324fc97e1
Puls02 <andrea.puls17@gmail.com>
2024-04-24 15:53:43 +0200

    Codice della pagina di registrazione quasi terminato

---
+++ js/registration.js
@@ -0,0 +1,23 @@
+function checkType(){
+    v=document.getElementById("abbonamento").value;
+    if(v == ""){
+        alert("Devi selezionare un tipo di abbonamento");
+        return false
+    }
+}
+
+function checkSubscription(){
+    checkLevel=document.getElementById("listastatus").value;
+    checkboxes=document.querySelectorAll("input[type='checkbox'][name='corso']");
+    if(checkLevel==""){
+        checkboxes.forEach(checkbox => {
+            if(checkbox.checked){
+                checkbox.checked=false;
+            }
+        });
+        alert("Devi selezionare il livello di abbonamento");
+        return false;
+    }
+
+    
+}
