25f4bd37fda25d61c6b3f069ae9bf15b42a6378e
eleonora <eleonora.marino2002@gmail.com>
2024-04-26 13:38:57 +0200

    modifica login

--- js/login.js
+++ js/login.js
@@ -5,3 +5,27 @@ const nav=document.querySelector('.login_menu');
 show_menu.onclick=()=>{
     nav.classList.toggle("show");
 };
+
+// Funzione per mostrare il pop-up
+function mostraPopUp() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Funzione per nascondere il pop-up
+function nascondiPopUp() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// funzione per la visbilità del campo password
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password");
+    var toggleIcon = document.querySelector(".password-toggle");
+    
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleIcon.src = "../hidden.png"; // Cambia il percorso dell'immagine per l'occhio chiuso
+    } else {
+      passwordField.type = "password";
+      toggleIcon.src = "../eye.png"; // Cambia il percorso dell'immagine per l'occhio aperto
+    }
+}
