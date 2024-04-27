283968dc8a68275cd67bc8e3e606b46d5ca18aef
eleonora <eleonora.marino2002@gmail.com>
2024-04-27 10:55:48 +0200

    popup funzionante

--- js/login.js
+++ js/login.js
@@ -6,61 +6,6 @@ show_menu.onclick=()=>{
     nav.classList.toggle("show");
 };
 
-// Funzione per mostrare il pop-up
-function mostraPopUp() {
-    document.getElementById("popup").style.display = "block";
-}
-
-// Funzione per nascondere il pop-up
-function nascondiPopUp() {
-    document.getElementById("popup").style.display = "none";
-}
-
-// funzione per la visbilità del campo password
-function togglePasswordVisibility() {
-    var passwordField = document.getElementById("password");
-    var toggleIcon = document.querySelector(".password-toggle");
-    
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      toggleIcon.src = "../hidden.png"; // Cambia il percorso dell'immagine per l'occhio chiuso
-    } else {
-      passwordField.type = "password";
-      toggleIcon.src = "../eye.png"; // Cambia il percorso dell'immagine per l'occhio aperto
-    }
-}
-
-// Aggiungi un ascoltatore di eventi per controllare se il campo è vuoto o meno
-document.addEventListener('DOMContentLoaded', function() {
-  var inputs = document.querySelectorAll('input');
-  inputs.forEach(function(input) {
-    input.addEventListener('input', function() {
-      if (this.value !== '') {
-        this.classList.add('filled');
-      } else {
-        this.classList.remove('filled');
-      }
-    });
-  });
-});
-
-// Funzione per mostrare il popup con una pagina web
-function mostraPopup() {
-  // Creazione dell'elemento iframe
-  var iframe = document.createElement('iframe');
-  iframe.src = '../login_registrazione/login.html'; // Sostituisci 'pagina_web_da_includere.html' con il percorso alla pagina web da includere
-  iframe.className = 'popup';
-
-  // Aggiunta dell'iframe al corpo del documento
-  document.body.appendChild(iframe);
-
-  // Aggiunta di un gestore per chiudere il popup al clic su di esso
-  iframe.addEventListener('click', function() {
-      document.body.removeChild(iframe);
-  });
-}
-
-
 // Funzione per collegare il bottone al popup
 function collegaBottoneAPopup() {
   // Seleziona il bottone desiderato
@@ -77,3 +22,50 @@ function collegaBottoneAPopup() {
 window.onload = function() {
   collegaBottoneAPopup();
 };
+
+// Funzione per mostrare il popup con una pagina web
+function mostraPopup() {
+  // Creazione dell'elemento iframe
+  var iframe = document.createElement('iframe');
+  iframe.src = '../login_registrazione/login.html'; // Sostituisci 'pagina_web_da_includere.html' con il percorso alla pagina web da includere
+  iframe.className = 'popup';
+
+  // Aggiunta dell'iframe al corpo del documento
+  document.body.appendChild(iframe);
+
+  // Aggiungo un gestore per il clic sul bottone di chiusura all'interno dell'iframe
+  iframe.onload = function() {
+    var closeButton = iframe.contentDocument.getElementById('closeButton');
+    closeButton.addEventListener('click', function() {
+      iframe.remove();
+    });
+  };
+}
+
+// funzione per la visbilità del campo password
+function togglePasswordVisibility() {
+  var passwordField = document.getElementById("password");
+  var toggleIcon = document.querySelector(".password-toggle");
+  
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    toggleIcon.src = "../hidden.png"; // Cambia il percorso dell'immagine per l'occhio chiuso
+  } else {
+    passwordField.type = "password";
+    toggleIcon.src = "../eye.png"; // Cambia il percorso dell'immagine per l'occhio aperto
+  }
+}
+
+// Aggiungi un ascoltatore di eventi per controllare se il campo è vuoto o meno
+document.addEventListener('DOMContentLoaded', function() {
+  var inputs = document.querySelectorAll('input');
+  inputs.forEach(function(input) {
+    input.addEventListener('input', function() {
+      if (this.value !== '') {
+        this.classList.add('filled');
+      } else {
+        this.classList.remove('filled');
+      }
+    });
+  });
+});
---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+/* JS per la nav bar*/
+
+// Funzione per mostrare il pop-up
+function mostraPopUp() {
+    document.getElementById("popup").style.display = "block";
+}
+  
+// Funzione per nascondere il pop-up
+function nascondiPopUp() {
+    document.getElementById("popup").style.display = "none";
+}
