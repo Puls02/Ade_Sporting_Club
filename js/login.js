33cd263b27e5b2ad03a061f689671bfa9001a626
eleonora <eleonora.marino2002@gmail.com>
2024-05-04 12:25:26 +0200

    popup funzionante dalla pagina attivita

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 //Apro il menù laterale
+/*
 const show_menu= document.querySelector('.person');
 const nav=document.querySelector('.login_menu');
 
 show_menu.onclick=()=>{
     nav.classList.toggle("show");
 };
+*/
 
 // Attivo la funzione
 window.onload = function() {
--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,12 @@
 /* JS per la nav bar*/
 
+const show_menu= document.querySelector('.person');
+const nav=document.querySelector('.login_menu');
+
+show_menu.onclick=()=>{
+    nav.classList.toggle("show");
+};
+
 // Funzione per mostrare il pop-up
 function mostraPopUp() {
     document.getElementById("popup").style.display = "block";
