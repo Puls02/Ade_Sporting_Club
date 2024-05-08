f7e21bc80d1a8002c00e95318a3bae004b8e2e53
Puls02 <andrea.puls17@gmail.com>
2024-05-08 18:55:49 +0200

    Iniziata parte di logout e fatta lettura dati da database su pagine utenti

--- js/navbar.js
+++ js/navbar.js
@@ -6,13 +6,3 @@ const nav=document.querySelector('.login_menu');
 show_menu.onclick=()=>{
     nav.classList.toggle("show");
 };
-
-// Funzione per mostrare il pop-up
-function mostraPopUp() {
-    document.getElementById("popup").style.display = "block";
-}
-  
-// Funzione per nascondere il pop-up
-function nascondiPopUp() {
-    document.getElementById("popup").style.display = "none";
-}
